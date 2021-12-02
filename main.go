7f94d4ba7ce27606c1d8e525487f18e604f9dc9e
Alex <94998782+testKhaiLymarenko@users.noreply.github.com>
2021-12-02 22:08:14 +0200

    2nd day = total mess

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
+
+/*
+	1. Начального значения если нет - ошибка  == ибо это первый месяц
+	2. Проверить, чтобы у всех клеточек было проверено их максимальное и начальное значение  - отнять только от последнего значения,
+		если его нет  - то значит месяц пустой  = (если хотя бы у одного аккаунта есть данные значит это не ошибка, а $0.00)
+	3. Структура
+*/
 
 func main() {
 	exFile, err := excelize.OpenFile("d:\\list.xlsx")
@@ -16,34 +24,30 @@ func main() {
 	}
 
 	IS_PVPRO, NOT_PVPRO := true, false
-	logins := [...]string{"...", ".", "d..", "de..", "d."}
-	lastCell, err := getLastCell(exFile)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	logins := [...]string{"l...", "r...l", "d...9", "..001", "..."}
 
 	roulette := map[string]string{
-		"B2": "B" + strconv.Itoa(lastCell), //wtfskins
-		"C2": "C" + strconv.Itoa(lastCell), //csgolines
-		"D2": "D" + strconv.Itoa(lastCell), //pvpro
+		"B2": "B", //wtfskins
+		"C2": "C", //csgolines
+		"D2": "D", //pvpro
 	}
 
 	for _, login := range logins {
-		for first, last := range roulette {
-			firstCellValue, _ := exFile.GetCellValue(login, first)
-			lastCellValue, _ := exFile.GetCellValue(login, last)
+		for firstCellName, letter := range roulette {
+			firstCellValue, _ := exFile.GetCellValue(login, firstCellName)
+			lastCellValue, _ := exFile.GetCellValue(login, getLastCellS(exFile, login, letter))
 
-			switch first {
+			switch firstCellName {
 			case "B2":
-				fmt.Printf("%s wtfskins: $%.2f\n", login, getIncome(firstCellValue, lastCellValue, NOT_PVPRO))
+				fmt.Printf("%s wtfskins: $%.2f\n", login,
+					getIncome(exFile, login, letter, firstCellValue, lastCellValue, NOT_PVPRO))
 			case "C2":
-				fmt.Printf("%s csgolive: $%.2f\n", login, getIncome(firstCellValue, lastCellValue, NOT_PVPRO))
+				fmt.Printf("%s csgolive: $%.2f\n", login,
+					getIncome(exFile, login, firstCellName, letter, firstCellValue, lastCellValue, NOT_PVPRO))
 			case "D2":
 				fmt.Printf("%s pvpro:    $%.2f (%.0f coins) \n", login,
-					getIncome(firstCellValue, lastCellValue, IS_PVPRO),
-					getIncome(firstCellValue, lastCellValue, IS_PVPRO)*100)
+					getIncome(exFile, login, letter, firstCellValue, lastCellValue, IS_PVPRO),
+					getIncome(exFile, login, letter, firstCellValue, lastCellValue, IS_PVPRO)*100)
 			}
 
 		}
@@ -53,14 +57,15 @@ func main() {
 	fmt.Scanln()
 }
 
-func getLastCell(exFile *excelize.File) (int, error) {
+func getLastCellS(exFile *excelize.File, login, letter string) string {
 
 	lastCell := 0
 	for i := 2; i <= 33; i++ {
-		cell, err := exFile.GetCellValue("lemon3100_", "B"+strconv.Itoa(i))
+		cell, err := exFile.GetCellValue(login, letter+strconv.Itoa(i))
 
 		if err != nil {
-			return 0, err
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		if cell == "" {
@@ -68,33 +73,41 @@ func getLastCell(exFile *excelize.File) (int, error) {
 		}
 	}
 
-	return lastCell, nil
+	return letter + strconv.Itoa(lastCell)
 }
 
-func getIncome(first, last string, isPVPRO bool) float64 {
+func getIncome(exFile *excelize.File, login, letter, firstVal, lastVal string, isPVPRO bool) float64 {
 
 	if !isPVPRO {
-		f, _ := strconv.ParseFloat(first[1:], 64)
-		l, _ := strconv.ParseFloat(last[1:], 64)
 
-		return l - f
+		i, _ := strconv.Atoi(first[1:])
+		n, _ := strconv.Atoi(last[1:])
+
+		var additional float64 = 0
+
+		for ; i <= n; i++ {
+			cell, _ := exFile.GetCellValue(login, letter+strconv.Itoa(i))
+
+			if strings.Contains(cell, "->") {
+				//$0.29 -> $0.02
+				part1, _ := strconv.ParseFloat(cell[1:strings.Index(cell, " ->")], 64)
+				part2, _ := strconv.ParseFloat(cell[strings.Index(cell, " $")+2:], 64)
+
+				additional += (part1 - part2)
+			}
+
+		}
+
+		f, _ := strconv.ParseFloat(firstVal[1:], 64)
+		l, _ := strconv.ParseFloat(lastVal[1:], 64)
+
+		return l - f + additional
 	} else {
 		//4433 coins + 560 crystals
 		fcoins, _ := strconv.Atoi(first[:strings.Index(first, " ")])
 		lcoins, _ := strconv.Atoi(last[:strings.Index(last, " ")])
 
 		return float64(lcoins-fcoins) / 100.0
-
-		/*fcrystals, _ := strconv.Atoi(first[strings.Index(first, "+ ")+2:strings.Index(first, " cr")])
-		lcrystals, _ := strconv.Atoi(last[strings.Index(first, "+ ")+2:strings.Index(last, " cr")])
-
-		crystals := lcrystals - fcrystals
-
-		if crystals == 0 {
-			return strconv.Itoa(coins)
-		} else {
-			return strconv.Itoa(coins) + " + " + strconv.Itoa(crystals)
-		}*/
 	}
 
 }

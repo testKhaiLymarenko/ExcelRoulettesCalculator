5c943bfedc435f68651e38b08279bd3a8e560b58
Alex <94998782+testKhaiLymarenko@users.noreply.github.com>
2021-12-03 23:12:35 +0200

    +checkFirstCell,+struct,+getLastCell -> Success!

--- main.go
+++ main.go
@@ -16,51 +16,171 @@ import (
 	3. Структура
 */
 
+var logins = [...]string{"lemon3100_", "rabascal", "destoki2019", "destoki2001", "destoki2002"}
+
+const (
+	WTFSKINS_L = "B"
+	CSGOLIVE_L = "C"
+	PVPRO_L    = "D"
+)
+
+type Accounts struct {
+	login            string
+	wtfskinsFirstVal float64 //dollars
+	wtfskinsLastVal  float64
+	csgoliveFirstVal float64
+	csgoliveLastVal  float64
+	pvproFirstVal    int //coins
+	pvproLastVal     int
+}
+
 func main() {
 	exFile, err := excelize.OpenFile("d:\\list.xlsx")
+
 	if err != nil {
 		fmt.Println(err)
+		fmt.Scanln()
 		return
 	}
 
-	IS_PVPRO, NOT_PVPRO := true, false
-	logins := [...]string{"l...", "r...l", "d...9", "..001", "..."}
-
-	roulette := map[string]string{
-		"B2": "B", //wtfskins
-		"C2": "C", //csgolines
-		"D2": "D", //pvpro
+	accounts := []Accounts{
+		{login: "l...."},
+		{login: "r...."},
+		{login: "d...9"},
+		{login: "d....1"},
+		{login: "de.."},
 	}
 
-	for _, login := range logins {
-		for firstCellName, letter := range roulette {
-			firstCellValue, _ := exFile.GetCellValue(login, firstCellName)
-			lastCellValue, _ := exFile.GetCellValue(login, getLastCellS(exFile, login, letter))
+	checkFirstCells(exFile)
+	getLastCellName(exFile, &accounts)
 
-			switch firstCellName {
-			case "B2":
-				fmt.Printf("%s wtfskins: $%.2f\n", login,
-					getIncome(exFile, login, letter, firstCellValue, lastCellValue, NOT_PVPRO))
-			case "C2":
-				fmt.Printf("%s csgolive: $%.2f\n", login,
-					getIncome(exFile, login, firstCellName, letter, firstCellValue, lastCellValue, NOT_PVPRO))
-			case "D2":
-				fmt.Printf("%s pvpro:    $%.2f (%.0f coins) \n", login,
-					getIncome(exFile, login, letter, firstCellValue, lastCellValue, IS_PVPRO),
-					getIncome(exFile, login, letter, firstCellValue, lastCellValue, IS_PVPRO)*100)
+	/*
+			IS_PVPRO, NOT_PVPRO := true, false
+		roulette := map[string]string{
+			"B2": "B", //wtfskins
+			"C2": "C", //csgolines
+			"D2": "D", //pvpro
+		}
+
+		for _, login := range logins {
+			for firstCellName, letter := range roulette {
+				firstCellValue, _ := exFile.GetCellValue(login, firstCellName)
+				lastCellValue, _ := exFile.GetCellValue(login, getLastCellS(exFile, login, letter))
+
+				switch firstCellName {
+				case "B2":
+					fmt.Printf("%s wtfskins: $%.2f\n", login,
+						getIncome(exFile, login, letter, firstCellValue, lastCellValue, NOT_PVPRO))
+				case "C2":
+					fmt.Printf("%s csgolive: $%.2f\n", login,
+						getIncome(exFile, login, firstCellName, letter, firstCellValue, lastCellValue, NOT_PVPRO))
+				case "D2":
+					fmt.Printf("%s pvpro:    $%.2f (%.0f coins) \n", login,
+						getIncome(exFile, login, letter, firstCellValue, lastCellValue, IS_PVPRO),
+						getIncome(exFile, login, letter, firstCellValue, lastCellValue, IS_PVPRO)*100)
+				}
+
+			}
+			fmt.Println()
+		}
+
+		fmt.Scanln()*/
+}
+
+func getLastCellName(exFile *excelize.File, accounts *[]Accounts) {
+
+	getLastCellValue := func(account *Accounts) {
+
+		for i := 'B'; i <= 'D'; i++ {
+
+			for j := 34; j > 2; j-- {
+				cell, _ := exFile.GetCellValue(account.login, string(i)+strconv.Itoa(j))
+				if cell != "" {
+
+					if i == 'B' || i == 'C' { //wtfskins and csgolive
+
+						if val, err := strconv.ParseFloat(cell[1:], 64); err != nil {
+							continue
+						} else {
+							if i == 'B' {
+								account.wtfskinsLastVal = val
+							} else if i == 'C' {
+								account.csgoliveLastVal = val
+							}
+							break
+						}
+
+					} else if i == 'D' { //pvpro
+
+						//4433 coins + 560 crystals
+						if val, err := strconv.Atoi(cell[:strings.Index(cell, " ")]); err != nil {
+							continue
+						} else {
+							account.pvproLastVal = val
+							break
+						}
+
+					}
+
+				}
+
 			}
 
 		}
-		fmt.Println()
+
 	}
 
-	fmt.Scanln()
+	//cuz value is copied but not &
+	for i, _ := range *accounts {
+		getLastCellValue(&(*accounts)[i])
+	}
 }
 
-func getLastCellS(exFile *excelize.File, login, letter string) string {
+func checkFirstCells(exFile *excelize.File) {
+
+	var buff strings.Builder
+
+	for _, login := range logins {
+
+		var empty bool
+		var str string
+		cell, _ := exFile.GetCellValue(login, WTFSKINS_L+"2")
+
+		if cell == "" {
+			empty = true
+			str = "wtfskins "
+		}
+
+		cell, _ = exFile.GetCellValue(login, CSGOLIVE_L+"2")
+
+		if cell == "" {
+			empty = true
+			str += "csgolives "
+		}
+
+		cell, _ = exFile.GetCellValue(login, PVPRO_L+"2")
+
+		if cell == "" {
+			empty = true
+			str += "pvpro"
+		}
+
+		if empty {
+			buff.WriteString(login + ": " + str + "\n")
+		}
+	}
+
+	if buff.String() != "" {
+		fmt.Printf("Error! List of cells that doesn't contain last month data in the first raw:\n%s", buff.String())
+		fmt.Scanln()
+		os.Exit(1)
+	}
+}
+
+/*func getLastCellS(exFile *excelize.File, login, letter string) string {
 
 	lastCell := 0
-	for i := 2; i <= 33; i++ {
+	for i := 2; i <= 34; i++ {
 		cell, err := exFile.GetCellValue(login, letter+strconv.Itoa(i))
 
 		if err != nil {
@@ -110,4 +230,4 @@ func getIncome(exFile *excelize.File, login, letter, firstVal, lastVal string, i
 		return float64(lcoins-fcoins) / 100.0
 	}
 
-}
+}*/

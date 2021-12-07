c1a5124fc12779a9d0a1be441e83ee82f5aa3038
Alex <94998782+testKhaiLymarenko@users.noreply.github.com>
2021-12-07 18:23:20 +0200

    fixed income in struct, added write to file (all roulettes + overall)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/fatih/color"
 	"github.com/xuri/excelize/v2"
@@ -17,46 +18,102 @@ func main() {
 	}
 
 	accounts := []Accounts{
-		{login: "..."},
-		{login: "..."},
-		{login: "..."},
-		{login: "d..."},
-		{login: "d..."},
+		{login: "le.."},
+		{login: "ra.."},
+		{login: "d...9."},
+		{login: "d....1"},
+		{login: "d...."},
 	}
 
 	var totalWtfskinsIncome, totalCsgolivesIncome, totalPvproDollarsIncome float64
 	var totalPvproCoinsIncome int
+	var totalOverallIncome float64
 
+	//both functions store data to the struct
 	checkAndGetFirstCells(exFile, &accounts)
 	getLastCellName(exFile, &accounts)
 
 	//Print income of each account and count the total income in loop to print it later
-	for _, account := range accounts {
-		switch account.login {
-		case "....":
-			color.Red(account.CalculateS())
-		case ".....":
-			color.Magenta(account.CalculateS())
-		case "d...":
-			color.Yellow(account.CalculateS())
-		case "..":
-			color.Cyan(account.CalculateS())
+	for i := range accounts {
+		switch accounts[i].login { //index is needed cuz range-loop copies accounts[i] to account, but not a pointer
+		case "...":
+			color.Red(accounts[i].CalculateS())
+		case "..l":
+			color.Magenta(accounts[i].CalculateS())
+		case "de...1...":
+			color.Yellow(accounts[i].CalculateS())
 		case "d.":
-			color.White(account.CalculateS())
+			color.Cyan(accounts[i].CalculateS())
+		case "d....":
+			color.White(accounts[i].CalculateS())
 		}
 
-		totalWtfskinsIncome += account.wtfskinsIncome
-		totalCsgolivesIncome += account.csgoliveIncome
+		totalWtfskinsIncome += accounts[i].wtfskinsIncome
+		totalCsgolivesIncome += accounts[i].csgoliveIncome
 
-		totalPvproCoinsIncome += account.pvproCoinsIncome
-		totalPvproDollarsIncome += account.pvproDollarsIncome
+		totalPvproCoinsIncome += accounts[i].pvproCoinsIncome
+		totalPvproDollarsIncome += accounts[i].pvproDollarsIncome
 
 	}
 
 	//print Total Income
+	totalOverallIncome = totalWtfskinsIncome + totalCsgolivesIncome + totalPvproDollarsIncome
 	color.Green("\nTotal Income (%d accounts):\n\n\twtfskins:  $%.2f\n\tcsgolives: $%.2f\n\tpvpro:     $%.2f (%d coins)\n\nOverall:   $%.2f\n",
-		len(accounts), totalWtfskinsIncome, totalCsgolivesIncome, totalPvproDollarsIncome, totalPvproCoinsIncome,
-		(totalWtfskinsIncome + totalCsgolivesIncome + totalPvproDollarsIncome))
+		len(accounts), totalWtfskinsIncome, totalCsgolivesIncome, totalPvproDollarsIncome, totalPvproCoinsIncome, totalOverallIncome)
+
+	incomeSheetName := exFile.GetSheetName(0)
+
+	for i := 'B'; i <= 'D'; i++ {
+		for j := range accounts {
+
+			if i == 'B' {
+				err := exFile.SetCellValue(incomeSheetName, "B"+strconv.Itoa(j+2),
+					fmt.Sprintf("+$%.2f", accounts[j].wtfskinsIncome))
+
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else if i == 'C' {
+				err := exFile.SetCellValue(incomeSheetName, "C"+strconv.Itoa(j+2),
+					fmt.Sprintf("+$%.2f", accounts[j].csgoliveIncome))
+
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else if i == 'D' {
+				err := exFile.SetCellValue(incomeSheetName, "D"+strconv.Itoa(j+2),
+					"+"+strconv.Itoa(accounts[j].pvproCoinsIncome)+" coins")
+
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	}
+
+	if err := exFile.Save(); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := exFile.SetCellValue(incomeSheetName, "B8", fmt.Sprintf("+$%.2f", totalWtfskinsIncome)); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := exFile.SetCellValue(incomeSheetName, "C8", fmt.Sprintf("+$%.2f", totalCsgolivesIncome)); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := exFile.SetCellValue(incomeSheetName, "D8", fmt.Sprintf("+%d coins (+$%.2f)", totalPvproCoinsIncome, totalPvproDollarsIncome)); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := exFile.SetCellValue(incomeSheetName, "C11", fmt.Sprintf("Total Income: $%.2f", totalOverallIncome)); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := exFile.Save(); err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Scanln()
 }

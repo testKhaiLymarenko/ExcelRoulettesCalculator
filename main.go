85ef443f060f73914910ee66e2df3da7310e9c00
Alex <94998782+testKhaiLymarenko@users.noreply.github.com>
2021-12-10 18:56:39 +0200

    added json parse of privat24 api and store data to xls file

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -11,7 +13,7 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-//Проверять есть ли значение в клетке - и если оно есть и не такое же точно - уведомить об этом, если одинаковы то не записывать
+//брать курс валют с НБУ
 func main() {
 
 	workFolder := "D:\\Program Files\\MEGAsync\\MEGAsync\\Internet Deals\\Steam\\ルーレット"
@@ -38,16 +40,16 @@ func main() {
 	}
 
 	accounts := []Accounts{
-		{login: "l///"},
-		{login: "r....."},
-		{login: "d....."},
-		{login: "d....."},
+		{login: "l......_"},
+		{login: ".....l"},
+		{login: "de....9"},
 		{login: "d...."},
+		{login: "....."},
 	}
 
 	var totalWtfskinsIncome, totalCsgolivesIncome, totalPvproDollarsIncome float64
 	var totalPvproCoinsIncome int
-	var totalOverallIncome float64
+	var totalOverallIncomeInDollars float64
 
 	//both functions store data to the struct
 	checkAndGetFirstCells(exFile, &accounts)
@@ -56,15 +58,15 @@ func main() {
 	//Print income of each account and count the total income in loop to print it later
 	for i := range accounts {
 		switch accounts[i].login { //index is needed cuz range-loop copies accounts[i] to account, but not a pointer
-		case "l.....":
+		case "....._":
 			color.Red(accounts[i].CalculateS())
 		case "r.....":
 			color.Magenta(accounts[i].CalculateS())
-		case "d.....":
+		case "d...":
 			color.Yellow(accounts[i].CalculateS())
-		case ".....":
+		case "de....":
 			color.Cyan(accounts[i].CalculateS())
-		case "d.....":
+		case "d......2":
 			color.White(accounts[i].CalculateS())
 		}
 
@@ -77,9 +79,9 @@ func main() {
 	}
 
 	//print Total Income
-	totalOverallIncome = totalWtfskinsIncome + totalCsgolivesIncome + totalPvproDollarsIncome
+	totalOverallIncomeInDollars = totalWtfskinsIncome + totalCsgolivesIncome + totalPvproDollarsIncome
 	color.Green("\nTotal Income (%d accounts):\n\n\twtfskins:  $%.2f\n\tcsgolives: $%.2f\n\tpvpro:     $%.2f (%d coins)\n\nOverall:   $%.2f\n",
-		len(accounts), totalWtfskinsIncome, totalCsgolivesIncome, totalPvproDollarsIncome, totalPvproCoinsIncome, totalOverallIncome)
+		len(accounts), totalWtfskinsIncome, totalCsgolivesIncome, totalPvproDollarsIncome, totalPvproCoinsIncome, totalOverallIncomeInDollars)
 
 	/*for {
 		fmt.Print("\n\nDo you want to store values to .xls file? (y/n): ")
@@ -145,7 +147,7 @@ func main() {
 		fmt.Println(err)
 	}
 
-	if err := exFile.SetCellValue(incomeSheetName, "C11", fmt.Sprintf("Total Income: $%.2f", totalOverallIncome)); err != nil {
+	if err := exFile.SetCellValue(incomeSheetName, "C11", fmt.Sprintf("Total Income: $%.2f", totalOverallIncomeInDollars)); err != nil {
 		fmt.Println(err)
 	}
 
@@ -194,20 +196,68 @@ func main() {
 
 	defer resp.Body.Close()
 
-	buff := make([]byte, 5000) //5000 characters should be enough for Privat24 Json response
-	resp.Body.Read(buff)
+	data, _ := ioutil.ReadAll(resp.Body)
 
 	//remove useless from the beginning ""date":"09.12.2021","bank":"PB","baseCurrency":980,"baseCurrencyLit":"UAH","exchangeRate":["
-	privatDataS := string(buff)[strings.Index(string(buff), "[")+1:]
-	fmt.Println(privatDataS)
+	//privatData := []byte(string(buff)[strings.Index(string(buff), "[")+1:])
+	data = []byte(string(data)[strings.Index(string(data), "[") : len(data)-1])
 
-	err = exFileIncome.SetCellValue(exFileIncome.GetSheetName(sheetYearIndex), "B"+strconv.Itoa(incomeMonth+1),
-		fmt.Sprintf("$%.2f", totalOverallIncome))
+	//"baseCurrency":"UAH","currency":"USD" "saleRate":27.4500000,"purchaseRate":27.0500000},
+	type Currency struct {
+		ForeignCurrencyName string  `json:"currency"`
+		PurchaseRate        float64 `json:"purchaseRate"`
+	}
 
-	if err != nil {
+	currencies := []Currency{}
+	if err = json.Unmarshal(data, &currencies); err != nil {
 		fmt.Println(err)
 		fmt.Scanln()
 		return
+	}
+
+	var totalOverallIncomeInHryvnia, totalOverallIncomeInRubles float64
+
+	for _, currency := range currencies {
+		if currency.ForeignCurrencyName == "USD" {
+			totalOverallIncomeInHryvnia = currency.PurchaseRate * totalOverallIncomeInDollars
+		}
+	}
+
+	//2 loops cuz we need to get income in hryvnia first
+	for _, currency := range currencies {
+		if currency.ForeignCurrencyName == "RUB" {
+			totalOverallIncomeInRubles = totalOverallIncomeInHryvnia / currency.PurchaseRate
+		}
+	}
+
+	//Set value for the Dollars cell
+	if err = exFileIncome.SetCellValue(exFileIncome.GetSheetName(sheetYearIndex), "B"+strconv.Itoa(incomeMonth+1),
+		fmt.Sprintf("$%.2f", totalOverallIncomeInDollars)); err != nil {
+
+		fmt.Println(err)
+		fmt.Scanln()
+		return
+
+	}
+
+	//Set value for the Rubles cell
+	if err = exFileIncome.SetCellValue(exFileIncome.GetSheetName(sheetYearIndex), "C"+strconv.Itoa(incomeMonth+1),
+		fmt.Sprintf("₽%.2f", totalOverallIncomeInRubles)); err != nil {
+
+		fmt.Println(err)
+		fmt.Scanln()
+		return
+
+	}
+
+	//Set value for the Hryvnia cell
+	if err = exFileIncome.SetCellValue(exFileIncome.GetSheetName(sheetYearIndex), "D"+strconv.Itoa(incomeMonth+1),
+		fmt.Sprintf("₴%.2f", totalOverallIncomeInHryvnia)); err != nil {
+
+		fmt.Println(err)
+		fmt.Scanln()
+		return
+
 	}
 
 	err = exFileIncome.Save()
